984734807506d6300e472164210b47989b1af901
Rivon <flavienrivon@gmail.com>
2024-06-26 12:33:18 +0300

    fix : config loading
    Config was unable to find the config.properties file

--- app/src/main/java/org/example/util/Config.java
+++ app/src/main/java/org/example/util/Config.java
@@ -7,7 +7,7 @@ public class Config {
 
     static {
         try {
-            properties.load(Config.class.getResourceAsStream("config.properties"));
+            properties.load(ClassLoader.getSystemResourceAsStream("config.properties"));
         } catch (Exception e) {
             throw new RuntimeException("Could not load config.properties file", e);
         }
